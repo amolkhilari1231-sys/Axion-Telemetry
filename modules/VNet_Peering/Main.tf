7766effc9d85b53d21adb76540e0179f22006b2d
Amol Khilari <amol.khilari1231@gmail.com>
2026-09-23 23:40:55 +0530

    diattach pips on vm

--- modules/VNet_Peering/Main.tf
+++ modules/VNet_Peering/Main.tf
@@ -1,5 +1,8 @@
 data "azurerm_virtual_network" "vnet_id" {
-    for_each = var.vnetpeering
+      for_each = {
+    for k, v in var.vnetpeering :
+    v.vnet_name => v
+  }
   name                = each.value.vnet_name
   resource_group_name = each.value.rg_name
   
@@ -10,5 +13,8 @@ resource "azurerm_virtual_network_peering" "vnetpeering" {
   name                      = each.value.name
   resource_group_name       = each.value.rg_name
   virtual_network_name      = each.value.vnet_name
-  remote_virtual_network_id = data.azurerm_virtual_network.vnet_id[each.key].id
+  remote_virtual_network_id = data.azurerm_virtual_network.vnet_id[each.value.remote_vnet_name].id
+
+   allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
 }
--- modules/Virtual_machine/Main.tf
+++ modules/Virtual_machine/Main.tf
@@ -31,13 +31,12 @@ resource "azurerm_network_interface" "nics" {
   ip_configuration {
     name                          = "internal"
     subnet_id                     = data.azurerm_subnet.subneet[each.key].id
-    public_ip_address_id          = data.azurerm_public_ip.pip_data[each.key].id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
 resource "azurerm_linux_virtual_machine" "VMS" {
-    depends_on = [ azurerm_network_interface.nics ]
+    depends_on = [ azurerm_network_interface.nics, azurerm_public_ip.pips ]
     for_each = var.vms
   name                = each.value.vm_name
   resource_group_name = each.value.rg_name
